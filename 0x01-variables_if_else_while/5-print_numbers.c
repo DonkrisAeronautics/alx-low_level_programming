db8c9288b8d9badd7925e3d54e90be4bed8633a5
donkrisaeronautics <donkris501@gmail.com>
2023-09-08 09:58:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+
+	{
+	if (number != '0' && number != '0')
+	putchar(number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
